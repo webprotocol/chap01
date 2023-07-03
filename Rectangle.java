a098fcb1b55660a730e01b4d63b02c187165a9ba
protocols <protocols@example.com>
2023-07-03 12:02:22 +0900

    first commit

---
+++ Rectangle.java
@@ -0,0 +1,14 @@
+
+
+class Rectangle {
+
+	public static void main(String[] args) {
+		System.out.printf("*****\n");
+		System.out.printf("*****\n");
+		System.out.printf("*****\n");
+		System.out.printf("*****\n");
+		System.out.printf("*****\n");
+		
+	}
+
+}
---
+++ Triangle.java
@@ -0,0 +1,12 @@
+
+class Triangle {
+
+	public static void main(String[] args) {
+		System.out.println("    *");
+		System.out.println("   **");
+		System.out.println("  ***");
+		System.out.println(" ****");
+		System.out.println("*****");
+	}
+
+}
