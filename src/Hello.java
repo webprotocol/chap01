08672b1153b19cede99b1b59692641533438764d
protocols <protocols@example.com>
2023-07-06 17:03:40 +0900

    Hello 수정

--- src/Hello.java
+++ src/Hello.java
@@ -6,9 +6,17 @@ public class Hello {
 		System.out.println("\033[31m");
 		System.out.println("\033[44m");
 
+		System.out.print("\033[10;20H");
 		System.out.println("Hello Java!");
 
+		System.out.println("\033[34m");
+		System.out.println("\033[41m");
+
+		System.out.print("\033[5;10H");
+		System.out.println("Hello JavaScript!");
+
 		System.out.println("\033[0m");
+		
 	}
 
 }
