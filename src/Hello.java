fd6690532edf5b5e55c6d495dc40963506415544
protocols <protocols@example.com>
2023-07-06 12:10:06 +0900

    color fg bg

--- src/Hello.java
+++ src/Hello.java
@@ -3,9 +3,12 @@ public class Hello {
 	public static void main(String[] args) {
 
 		System.out.println("\033[2J");
+		System.out.println("\033[31m");
+		System.out.println("\033[44m");
 
 		System.out.println("Hello Java!");
 
+		System.out.println("\033[0m");
 	}
 
 }
