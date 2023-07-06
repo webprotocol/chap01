085d2b5f3c753c43699a3f1428fa9279879af4ca
protocols <protocols@example.com>
2023-07-06 12:01:09 +0900

    vt100 clear

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("\033[2J");
+
+		System.out.println("Hello Java!");
+
+	}
+
+}
--- src/Triangle.java
+++ src/Triangle.java
@@ -2,6 +2,7 @@
 class Triangle {
 
 	public static void main(String[] args) {
+		// 삼각형 출력
 		System.out.println("@@@@@");
 		System.out.println("@@@@");
 		System.out.println("@@@");
